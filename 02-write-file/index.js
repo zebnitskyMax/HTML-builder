dc4e17a802d2427d5afd871d7fc2fcf3031242f9
Zebnitsky Max <Zebnitsky.Max87@gmail.com>
2024-01-23 22:39:32 +0300

    feat: complete task two

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin } = process;
+const name = path.basename('text.txt');
+const pathName = path.join(__dirname, name);
+
+const writeStream = fs.createWriteStream(pathName);
+
+const EventEmitter = require('events');
+const myEmmiter = new EventEmitter();
+const welcomUser = () => {
+  console.log('Hello my new reviewer.......)))!');
+};
+myEmmiter.on('greetingEvent', welcomUser);
+myEmmiter.emit('greetingEvent');
+// writeStream.end('readline');
+
+process.stdout.write('Hello reviewer, input text....!' + '\n');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  } else {
+    writeStream.write(data);
+  }
+  // data = data.toString();
+  // process.stdout.write(data + '\n');
+});
