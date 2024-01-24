1cd92d60604e7b2cdb2fa35e19752d6ce3416d37
Zebnitsky Max <Zebnitsky.Max87@gmail.com>
2024-01-24 20:57:55 +0300

    fix: repaired output

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,11 +3,26 @@ const path = require('path');
 const pathName = path.join(__dirname, 'secret-folder');
 console.log(pathName);
 fs.readdir(path.join(__dirname, 'secret-folder'), (err, data) => {
-  if (err) throw err;
+  if (err) {
+    err;
+    return;
+  }
   for (const el of data) {
     // console.log(path.extname(el));
     // console.log(path.basename(el));
     // console.log(el.name);
-    console.log(`${path.basename(el)} - ${path.extname(el)}`);
+    // console.log(`${path.basename(el)} - ${path.extname(el)}`);
+    fs.stat(path.join(pathName, el), (err, stats) => {
+      if (err) err;
+      if (stats.isFile) {
+        // console.log(el);
+        // console.log(stats.size);
+        if (stats.size) {
+          console.log(
+            `${path.basename(el)} - ${path.extname(el)} - ${stats.size}b`,
+          );
+        }
+      }
+    });
   }
 });
